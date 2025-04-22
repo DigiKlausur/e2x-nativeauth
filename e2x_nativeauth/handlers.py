32247b3e7b85c30cb064f75127ce81a7a4dcfd62
Tim Metzler <tim.metzler@h-brs.de>
2025-04-22 14:49:58 +0200

    Extract data from json body

--- e2x_nativeauth/handlers.py
+++ e2x_nativeauth/handlers.py
@@ -8,8 +8,15 @@ class E2XUsersAPIHandler(APIHandler):
     """
     @admin_users_scope
     async def post(self):
-        username = self.get_body_argument("username")
-        password = self.get_body_argument("password")
+        data = self.get_json_body()
+        if not data:
+            self.finish({"status": "error", "message": "No data provided"})
+            return
+        if "username" not in data or "password" not in data:
+            self.finish({"status": "error", "message": "Username and password are required"})
+            return
+        username = data["username"]
+        password = data["password"]
         self.authenticator.create_user(username, password)
         user = self.authenticator.get_user(username)
         user.is_authorized = True
