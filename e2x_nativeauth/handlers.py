24d19a14b6fb47a89d058592c2c716f2cbdd4774
Tim Metzler <tim.metzler@h-brs.de>
2025-04-22 14:48:28 +0200

    Access methods from authenticator

--- e2x_nativeauth/handlers.py
+++ e2x_nativeauth/handlers.py
@@ -10,8 +10,8 @@ class E2XUsersAPIHandler(APIHandler):
     async def post(self):
         username = self.get_body_argument("username")
         password = self.get_body_argument("password")
-        self.create_user(username, password)
-        user = self.get_user(username)
+        self.authenticator.create_user(username, password)
+        user = self.authenticator.get_user(username)
         user.is_authorized = True
-        self.db.commit()
+        self.authenticator.db.commit()
         self.finish({"status": "success", "message": "User added successfully"})
